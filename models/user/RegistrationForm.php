a833fd9e57b1c3e71f0dbb57d060120907a8cc03
hunwalk <horg3r@gmail.com>
2020-03-28 21:48:57 +0100

    feat: added location field to model

--- models/user/RegistrationForm.php
+++ models/user/RegistrationForm.php
@@ -2,7 +2,9 @@
 
 namespace app\models\user;
 
+use dektrium\user\models\Profile;
 use dektrium\user\models\RegistrationForm as BaseRegistrationForm;
+use dektrium\user\models\User;
 
 class RegistrationForm extends BaseRegistrationForm
 {
@@ -12,6 +14,11 @@ class RegistrationForm extends BaseRegistrationForm
     public $api_key;
 
     /**
+     * @var string $location
+     */
+    public $location;
+
+    /**
      * @inheritdoc
      */
     public function rules()
@@ -19,6 +26,30 @@ class RegistrationForm extends BaseRegistrationForm
         $rules = parent::rules();
         $rules[] = ['api_key', 'required'];
         $rules[] = ['api_key', 'string', 'max' => 16];
+        $rules[] = ['location', 'string'];
+        $rules[] = ['location', 'required'];
         return $rules;
+    }
+
+    public function attributeLabels()
+    {
+        $attributeLabels =  parent::attributeLabels();
+        $attributeLabels['location'] = \Yii::t('app', 'City');
+        return $attributeLabels;
+    }
+
+    public function loadAttributes(User $user)
+    {
+        $user->setAttributes([
+            'email'    => $this->email,
+            'username' => $this->username,
+            'password' => $this->password,
+        ]);
+        /** @var Profile $profile */
+        $profile = \Yii::createObject(Profile::className());
+        $profile->setAttributes([
+            'location' => $this->location,
+        ]);
+        $user->setProfile($profile);
     }
 }
